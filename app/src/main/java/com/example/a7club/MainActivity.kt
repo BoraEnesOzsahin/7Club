6df77b7c5c32c109128fdeed9907e352ca0e848b
freed <freedomsami73@gmail.com>
2025-11-11 12:35:40 +0300

    i change greeting message android to helooooo .

--- app/src/main/java/com/example/a7club/MainActivity.kt
+++ app/src/main/java/com/example/a7club/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             _7ClubTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "HELLOOOOO",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
