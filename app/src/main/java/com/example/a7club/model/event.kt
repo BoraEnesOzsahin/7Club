6d5c9e9d93f2f962723f2c6319cd27987094879e
freed <freedomsami73@gmail.com>
2025-12-14 01:45:35 +0300

    a

--- app/src/main/java/com/example/a7club/model/event.kt
+++ app/src/main/java/com/example/a7club/model/event.kt
@@ -1,12 +1,6 @@
 package com.example.a7club.model
 
 data class Event(
-<<<<<<< HEAD
-    val id: String,
-    val title: String
-)
-=======
     val id: Int,
     val title: String
 )
->>>>>>> 3cb380e87be89806b92c731169039784690ee0c8
