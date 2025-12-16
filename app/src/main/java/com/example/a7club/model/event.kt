9dcac813963d10fce67c5d3f420d97048d15b076
Yagmur Direkci <yagmurdirekci@gmail.com>
2025-12-16 13:11:56 +0300

    event changed

--- app/src/main/java/com/example/a7club/model/event.kt
+++ app/src/main/java/com/example/a7club/model/event.kt
@@ -1,10 +1,6 @@
 package com.example.a7club.model
 
 data class Event(
-    val id: String = "",
-    val title: String = "",
-    val description: String = "",
-    val clubName: String = "",
-    val date: Long = 0L,
-    val imageUrl: String? = null
+    val id: Int,
+    val title: String
 )
