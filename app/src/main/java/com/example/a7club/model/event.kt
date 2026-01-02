dc4218ace0f80f6a8a2542fcb0fb5b12c810b9d1
freed <freedomsami73@gmail.com>
2026-01-02 17:44:44 +0300

    Database yapılıyor.

--- app/src/main/java/com/example/a7club/model/event.kt
+++ app/src/main/java/com/example/a7club/model/event.kt
@@ -1,23 +1,6 @@
 package com.example.a7club.model
 
-import com.google.firebase.Timestamp
-import com.google.firebase.firestore.DocumentId
-
 data class Event(
-    @DocumentId val id: String = "",
-    val title: String = "",
-    val description: String = "",
-
-    // --- İŞTE EKSİK OLAN KISIM BURASIYDI ---
-    val eventTime: String = "",      // Bunu eklemezsen hata alırsın
-    // ---------------------------------------
-
-    val contactPhone: String = "",
-    val location: String = "",
-    val clubId: String = "",
-    val clubName: String = "",
-    val timestamp: Timestamp? = null,
-    val status: String = "PENDING",
-    val category: String = "General",
-    val formUrl: String = ""
+    val id: Int,
+    val title: String
 )
